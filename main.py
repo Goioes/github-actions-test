e0393bc3e1d958f353a51328316d590a143d7978
Giel <gieloosterbos3@gmail.com>
2025-03-30 00:59:33 +0100

    commit message

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 def greet():
     return "Hello from GitHub Actions!"
 
-
 if __name__ == "__main__":
+    dict = {"key": "value",
+            'bla': 'blub'}
     print(greet())
+
--- main_test.py
+++ main_test.py
@@ -1,6 +1,5 @@
 from main import greet
-import numpy as np
-
 
 def test_greet():
     assert greet() == "Hello from GitHub Actions!"
+    
